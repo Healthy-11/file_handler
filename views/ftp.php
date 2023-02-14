509674b3b88ceac846ee0facdc4544c50ebb5f10
Healthy-11 <merlin.lehning@gmail.com>
2023-02-14 14:15:59 +0100

    download documents

--- views/ftp.php
+++ views/ftp.php
@@ -40,12 +40,13 @@ if (isset($_POST['download']) && isset($_POST['dlfile'])) {
         $ftp = ftp_connect("focus.immo", 21);
         ftp_login($ftp, $_SESSION["config"]['ftp_username'], $_SESSION["config"]['ftp_password']);
         ftp_pasv($ftp, true);
-        $file = "tets.pdf";
-        $fp = fopen($file, 'w');
-        if (ftp_nb_fget($ftp, $fp, $_SESSION["code"] . "/signed/" . $file_name, FTP_BINARY)) {
-            echo "Successfully written to $file\n";
-        } else {
-            echo "There was a problem\n";
+        $local_file = "downloaded.pdf";
+        echo $file_name;
+        if (ftp_get($ftp, $local_file, $_SESSION["code"] . "/signed/" . $file_name, FTP_BINARY)) {
+            $file_new_name = $file_name;
+            header('Content-Type: application/pdf');
+            header("Content-Disposition: attachment; filename=\"" . $file_new_name . '"');
+            readfile($local_file);
         }
         ftp_close($ftp);
     } catch (Exception $e) {
@@ -70,27 +71,37 @@ if (isset($_POST['download']) && isset($_POST['dlfile'])) {
     </form>
 </div>
 <br>
-<form method="post">
-    <?php
-    $ftp = ftp_connect("focus.immo", 21);
-    ftp_login($ftp, $_SESSION["config"]['ftp_username'], $_SESSION["config"]['ftp_password']);
-    ftp_pasv($ftp, true);
-    $buff = ftp_rawlist($ftp, $_SESSION["code"] . "/signed/");
-    ftp_close($ftp);
-    foreach ($buff as $key): ?>
-        <?php $info = explode(" ", preg_replace('/\s+/', ' ', $key));
-        if ($info[8] == "." || $info[8] == "..") {
-            continue;
+
+<?php
+$ftp = ftp_connect("focus.immo", 21);
+ftp_login($ftp, $_SESSION["config"]['ftp_username'], $_SESSION["config"]['ftp_password']);
+ftp_pasv($ftp, true);
+$buff = ftp_rawlist($ftp, $_SESSION["code"] . "/signed/");
+ftp_close($ftp);
+foreach ($buff as $key): ?>
+    <?php $info = explode(" ", preg_replace('/\s+/', ' ', $key));
+    if ($info[8] == "." || $info[8] == "..") {
+        continue;
+    }
+    $name = "";
+    for ($i = 8; $i < sizeof($info); $i++) {
+        if ($i > 8) {
+            $name = $name . " " . $info[$i];
+        } else {
+            $name = $info[$i];
         }
-        ?>
-        <div class="tile">
-            <h3 class="list-h3"><?= $info[8] ?></h3>
-            <p class="list-p"><?= $info[5] . " " . $info[6] . " " . $info[7] ?></p>
-            <p class="list-p"><?= $info[4] . " B" ?></p>
-            <input type="hidden" name="dlfile" value="<?= $info[8] ?>"/>
+    }
+    ?>
+    <div class="tile">
+        <h3 class="list-h3"><?= $name ?></h3>
+        <p class="list-p"><?= $info[5] . " " . $info[6] . " " . $info[7] ?></p>
+        <p class="list-p"><?= $info[4] . " B" ?></p>
+        <form method="POST" action="">
+            <input type="hidden" name="dlfile" value="<?= $name ?>"/>
             <input type="submit" name="download" value="Download"/>
-        </div>
-    <?php endforeach; ?>
-    <form>
+        </form>
+    </div>
+<?php endforeach; ?>
+
 </body>
 </html>
